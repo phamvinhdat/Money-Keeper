7ae1b6856135799bf45ea975a4efee251e3f7355
pham vinh dat <phamvinhdat1998@gmail.com>
2018-11-14 19:58:08 +0700

    Update Money Keeper/EditCategoryInputViewController.swift

--- Money Keeper/EditCategoryInputViewController.swift
+++ Money Keeper/EditCategoryInputViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class EditCategoryInputViewController: UIViewController {
-
+    
+    private var segment:UISegmentedControl = UISegmentedControl()
+    var root:InputViewController? = nil
     override func viewDidLoad() {
         super.viewDidLoad()
-        let root = self.navigationController?.viewControllers[0]
-        self.navigationItem.titleView = root!.navigationItem.titleView
-       
+        setNavigationbar_viewDidLoad()
+        root = self.navigationController?.viewControllers[0] as? InputViewController
     }
 
+    func setNavigationbar_viewDidLoad(){
+        self.navigationItem.backBarButtonItem?.tintColor =  #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
+        segment.insertSegment(withTitle: "Expense", at: 0, animated: true)
+        segment.insertSegment(withTitle: "Income", at: 1, animated: true)
+        segment.tintColor = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
+        segment.selectedSegmentIndex = root!.isExpense ? 0 : 1
+        self.navigationItem.titleView = segment
+    }
+    
 }
